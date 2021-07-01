036a5f37b95f8c3f0fb88faf1569455ad55db30a
andrei_labadaev <53191183+ivolat05@users.noreply.github.com>
2021-07-01 15:34:13 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -33,13 +33,35 @@ window.addEventListener('wheel', function (e) {
     }
 })
 
-window.addEventListener('touchmove', function (e) {
-    i += 10;
-    img.style.transform = 'translateY( -' + i + '%)';
-    img12.style.transform = 'translateY( -' + i + '%)';
-    img2.style.transform = 'translateY( -' + i + '%)';
-    img22.style.transform = 'translateY( -' + i + '%)';
-    img3.style.transform = 'translateY( -' + i + '%)';
-    img32.style.transform = 'translateY( -' + i + '%)';
-    x++;
-})
+let event = null;
+document.addEventListener("touchstart", function (e) {
+    event = e;
+});
+document.addEventListener("touchmove", function (e) {
+    if (event) {
+        let start = e.touches[0].pageY - event.touches[0].pageY;
+        if (start > 0) {
+            if (x < 5) {
+                i += 40;
+                img.style.transform = 'translateY( ' + i + '%)';
+                img12.style.transform = 'translateY( ' + i + '%)';
+                img2.style.transform = 'translateY( -' + i + '%)';
+                img22.style.transform = 'translateY( -' + i + '%)';
+                img3.style.transform = 'translateY( ' + i + '%)';
+                img32.style.transform = 'translateY( ' + i + '%)';
+                x++;
+            }
+        } else {
+            if (x > 0) {
+                i -= 40;
+                img.style.transform = 'translateY( ' + i + '%)';
+                img12.style.transform = 'translateY( ' + i + '%)';
+                img2.style.transform = 'translateY( -' + i + '%)';
+                img22.style.transform = 'translateY( -' + i + '%)';
+                img3.style.transform = 'translateY( ' + i + '%)';
+                img32.style.transform = 'translateY( ' + i + '%)';
+                x--;
+            }
+        }
+    }
+});
